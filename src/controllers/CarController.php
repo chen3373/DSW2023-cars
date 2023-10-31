0ab62360e4d6f6dc4806984f74463b34ca491f09
Minghai Chen <chenminghai79@gmail.com>
2023-10-31 20:12:34 +0000

    read json with cars

--- src/controllers/CarController.php
+++ src/controllers/CarController.php
@@ -5,9 +5,14 @@ class CarController
   private $cars = [];
   public function __construct()
   {
-    $this->cars[] = new Car("4563", "Toyota", "Corolla", 1998, "green");
-    $this->cars[] = new Car("3453", "Nissan", "Nismo", 2021, "yellow");
-    $this->cars[] = new Car("2933", "Seat", "Ibiza", 2015, "red");
+    // $this->cars[] = new Car("4563", "Toyota", "Corolla", 1998, "green");
+    // $this->cars[] = new Car("3453", "Nissan", "Nismo", 2021, "yellow");
+    // $this->cars[] = new Car("2933", "Seat", "Ibiza", 2015, "red");
+    $json = file_get_contents('../data/cars.json');
+    $carsJson = json_decode($json);
+    foreach ($carsJson as $carJson) {
+      $this->cars[] = new Car($carJson->id, $carJson->make, $carJson->model, $carJson->year, $carJson->color);
+    }
   }
   public function list()
   {
